6c34d0f7bac7b4351385b7f3a284cb9efca55d89
Iko Raga <ikoraga@gmail.com>
2023-12-13 14:27:57 +0700

    feat: adding responsee success and failed DRUD

---
+++ js/class.js
@@ -0,0 +1,119 @@
+function addClass() {
+    const classNameValue = $("#className").val();
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/api/class",
+        contentType: "application/json",
+        data: JSON.stringify({ name: classNameValue }),
+        success: function (response) {
+            setTimeout(() => {
+                alert("sukses nenambah data kelas!");
+            }, 50)
+            loadClassList();
+            loadClassDropdown();
+
+        },
+        error: function (error) {
+            setTimeout(() => {
+                alert("gagal data kelas!");
+            }, 50)
+        }
+    });
+}
+
+function updateClass(classId) {
+    const updatedClassName = prompt("Enter the updated class name:", "");
+
+    if (updatedClassName !== null) {
+        $.ajax({
+            type: "PATCH",
+            url: `http://localhost:8080/api/class/${classId}`,
+            contentType: "application/json",
+            data: JSON.stringify({ name: updatedClassName }),
+            success: function (response) {
+                setTimeout(() => {
+                    alert("sukses ubah data kelas!");
+                }, 50)
+                loadClassList();
+                loadClassDropdown();
+            },
+            error: function (error) {
+                setTimeout(() => {
+                    alert("gagal nenambah data kelas!");
+                }, 50)
+            }
+        });
+    }
+}
+
+function loadClassList() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/class",
+        success: function (response) {
+            const classes = response.class;
+            const $classList = $("#classList");
+            $classList.empty();
+
+            classes.forEach(function (cls) {
+                const listItem = `<li class="list-group-item">${cls.name}
+                    <button class="btn btn-danger btn-sm ml-2" onclick="deleteClass('${cls.id}')">Hapus</button>
+                    <button class="btn btn-warning btn-sm ml-2" onclick="updateClass('${cls.id}')">Ubah</button>
+                </li>`;
+                $classList.append(listItem);
+            });
+        },
+        error: function (error) {
+            setTimeout(() => {
+                alert("gagal load data kelas!");
+            }, 50)
+        }
+    });
+}
+
+function loadClassDropdown() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/class",
+        success: function (response) {
+            const classes = response.class;
+            const $classDropdown = $("#classDropdown");
+            $classDropdown.empty();
+
+            classes.forEach(function (cls) {
+                $classDropdown.append(`<option value="${cls.id}">${cls.name}</option>`);
+            });
+        },
+        error: function (error) {
+            setTimeout(() => {
+                alert("gagal load data kelas!");
+            }, 50)
+        }
+    });
+}
+
+function deleteClass(classId) {
+    $.ajax({
+        type: "DELETE",
+        url: `http://localhost:8080/api/class/${classId}`,
+        success: function (response) {
+            setTimeout(() => {
+                alert("sukses hapus data kelas!");
+            }, 50)
+
+            loadClassList();
+            loadClassDropdown();
+        },
+        error: function (error) {
+            setTimeout(() => {
+                alert("gagal hapus data kelas!");
+            }, 50)
+        }
+    });
+}
+
+$(document).ready(function () {
+    loadClassList();
+    loadClassDropdown();
+});
---
+++ js/materials.js
@@ -0,0 +1,178 @@
+
+function addMaterial() {
+    var materialNameValue = $("#materialName").val();
+    var materialContentValue = tinymce.get('materialContent').getContent();
+    var selectedClassId = $("#classDropdown").val();
+
+    var requestData = {
+        name: materialNameValue,
+        content: materialContentValue,
+        classId: selectedClassId,
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/api/materials",
+        contentType: "application/json",
+        data: JSON.stringify(requestData),
+        success: function (response) {
+            setTimeout(() => {
+                alert("sukses nambah data material!");
+            }, 50)
+            $("#materialName").val('');
+            tinymce.get('materialContent').setContent('');
+            loadMaterialDropdown();
+            loadMaterialList();
+        },
+
+        error: function (error) {
+            setTimeout(() => {
+                alert("gagal nambah data material!");
+            }, 50)
+        }
+    });
+}
+function loadMaterialList() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/materials",
+        success: function (response) {
+            var materials = response.material;
+            $("#materialList").empty();
+            materials.forEach(function (material) {
+                $("#materialList").append('<li class="list-group-item">' + material.name +
+                    '<button class="btn-danger btn-sm ml-2" onclick="deleteMaterial(\'' + material.id + '\')">Hapus</button>' +
+                    '<button class="btn-warning btn-sm ml-2" onclick="populateMaterialForm(\'' + material.id + '\')">Ubah</button>' +
+                    '</li>');
+            });
+        },
+        error: function (error) {
+            setTimeout(() => {
+                alert("gagal load data material!");
+            }, 50)
+        }
+    });
+}
+
+function deleteMaterial(materialId) {
+    $.ajax({
+        type: "DELETE",
+        url: "http://localhost:8080/api/materials/" + materialId,
+        success: function (response) {
+            setTimeout(() => {
+                alert("sukses hapus data material!");
+            }, 50)
+            loadMaterialDropdown();
+            loadMaterialList();
+        },
+        error: function (error) {
+            setTimeout(() => {
+                alert("gagal nambah data material!");
+            }, 50)
+        }
+    });
+}
+
+function updateMaterial() {
+    var updatedMaterialId = $("#materialIdHidden").val();
+    var updatedMaterialName = $("#materialName").val();
+    var updatedMaterialContent = tinymce.get('materialContent').getContent();
+
+    if (!updatedMaterialId) {
+        setTimeout(() => {
+            alert("nama data material tidak diketahui!");
+        }, 50)
+        return;
+    }
+
+    var updatedData = {
+        id: updatedMaterialId,
+        name: updatedMaterialName,
+        content: updatedMaterialContent,
+    };
+
+    $.ajax({
+        type: "PATCH",
+        url: "http://localhost:8080/api/materials/" + updatedMaterialId,
+        contentType: "application/json",
+        data: JSON.stringify(updatedData),
+        success: function (response) {
+            setTimeout(() => {
+                alert("sukses ubah data material!");
+            }, 50)
+            loadMaterialDropdown();
+            loadMaterialList();
+
+            $("#materialName").val("");
+            tinymce.get('materialContent').setContent("");
+
+            $("#addMaterialBtn").text("Add Material");
+
+            $("#materialForm").off("submit");
+        },
+        error: function (error) {
+            setTimeout(() => {
+                alert("gagal nambah data material!");
+            }, 50)
+        }
+    });
+}
+
+function populateMaterialForm(materialId) {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/materials/" + materialId,
+        success: function (response) {
+            var material = response.material;
+            $("#materialName").val(material.name);
+            tinymce.get('materialContent').setContent(material.content);
+
+            $("#materialIdHidden").val(materialId);
+            $("#addMaterialBtn").text("Update Material");
+        },
+        error: function (error) {
+            setTimeout(() => {
+                alert("gagal mengambil data detail material!");
+            }, 50)
+        }
+    });
+}
+
+function loadMaterialDropdown() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/materials",
+        success: function (response) {
+            var materials = response.material;
+            $("#materialDropdown").empty();
+            materials.forEach(function (material) {
+                $("#materialDropdown").append('<option value="' + material.id + '">' + material.name + '</option>');
+            });
+        },
+        error: function (error) {
+            setTimeout(() => {
+                alert("gagal load data material!");
+            }, 50)
+        }
+    });
+}
+
+$(document).ready(function () {
+    tinymce.init({
+        selector: '#materialContent',
+        plugins: 'image',
+        a11y_advanced_options: true,
+        height: 300,
+        plugins: [
+            'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
+            'searchreplace', 'wordcount', 'visualblocks', 'visualchars', 'code', 'fullscreen', 'insertdatetime',
+            'media', 'table', 'emoticons', 'template', 'help'
+        ],
+        toolbar: 'undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | ' +
+            'bullist numlist outdent indent | link image | print preview media fullscreen | ' +
+            'forecolor backcolor emoticons | help',
+    });
+
+    loadMaterialList();
+    loadMaterialDropdown();
+});
