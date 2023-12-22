49caf0c6d502b14884216955c932e8a6a0a7d682
Iko Raga <ikoraga@gmail.com>
2023-12-22 08:30:08 +0700

    modify: change background dashboard and delete comment

--- js/login.js
+++ js/login.js
@@ -1,20 +1,17 @@
-// You can remove the DOMContentLoaded event listener if the script is placed just before </body>
-
-document.getElementById('loginButton').addEventListener('click', validateLogin);
+document.getElementById("loginButton").addEventListener("click", validateLogin);
 
 function validateLogin() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
-    // Check if both username and password are provided
-    if (!username || !password) {
-        alert('Data harus diisi!');
-        return;
-    }
+  if (!username || !password) {
+    alert("Data harus diisi!");
+    return;
+  }
 
-    if (username === 'admin' && password === 'admin') {
-        window.location.href = "../inputData.html";
-    } else {
-        alert('Password atau username salah, yuk coba lagi');
-    }
+  if (username === "admin" && password === "admin") {
+    window.location.href = "../inputData.html";
+  } else {
+    alert("Password atau username salah, yuk coba lagi");
+  }
 }
--- js/quiz.js
+++ js/quiz.js
@@ -1,54 +1,51 @@
 function addQuestion() {
-    var questionTextValue = $("#questionText").val();
-    var selectedMaterialId = $("#materialDropdown").val();
+  var questionTextValue = $("#questionText").val();
+  var selectedMaterialId = $("#materialDropdown").val();
 
-    // Collect options data
-    var options = [];
-    $("#options .option-group").each(function(index) {
-        var optionText = $(this).find("input[type='text']").val();
-        var isCorrect = $(this).find("input[type='checkbox']").prop("checked");
+  var options = [];
+  $("#options .option-group").each(function (index) {
+    var optionText = $(this).find("input[type='text']").val();
+    var isCorrect = $(this).find("input[type='checkbox']").prop("checked");
 
-        options.push({
-            option: String.fromCharCode(65 + index), // Convert index to A, B, C, ...
-            value: optionText,
-            isCorrect: isCorrect
-        });
+    options.push({
+      option: String.fromCharCode(65 + index), // Convert index to A, B, C, ...
+      value: optionText,
+      isCorrect: isCorrect,
     });
+  });
 
-    var requestData = {
-        questions: questionTextValue,
-        answer: options,
-        materialId: selectedMaterialId,
-    };
+  var requestData = {
+    questions: questionTextValue,
+    answer: options,
+    materialId: selectedMaterialId,
+  };
 
-    $.ajax({
-        type: "POST",
-        url: "http://localhost:8080/api/quiz",
-        contentType: "application/json",
-        data: JSON.stringify(requestData),
-        success: function (response) {
-            setTimeout(() => {
-                alert("sukses nambah data pertanyaan!");
-            }, 50)
-            $("#questionText").val('');
-            $("#options").empty();
-            displayQuestions();
-
-        },
-        error: function (error) {
-            setTimeout(() => {
-                alert("gagal nambah data pertanyaan!");
-            }, 50)
-        }
-    });
+  $.ajax({
+    type: "POST",
+    url: "http://34.87.40.161/api/quiz",
+    contentType: "application/json",
+    data: JSON.stringify(requestData),
+    success: function (response) {
+      setTimeout(() => {
+        alert("sukses nambah data pertanyaan!");
+      }, 50);
+      $("#questionText").val("");
+      $("#options").empty();
+      displayQuestions();
+    },
+    error: function (error) {
+      setTimeout(() => {
+        alert("gagal nambah data pertanyaan!");
+      }, 50);
+    },
+  });
 }
 
-// Function to add a new option field
 function addOption() {
-    var optionIndex = $("#options .option-group").length;
-    var optionChar = String.fromCharCode(65 + optionIndex); // Convert index to A, B, C, ...
+  var optionIndex = $("#options .option-group").length;
+  var optionChar = String.fromCharCode(65 + optionIndex); // Convert index to A, B, C, ...
 
-    var optionField = `
+  var optionField = `
         <div class="option-group mb-2">
             <div class="input-group">
                 <input type="text" class="form-control" placeholder="Pilihan ${optionChar}" required>
@@ -62,10 +59,9 @@ function addOption() {
             </div>
         </div>`;
 
-    $("#options").append(optionField);
+  $("#options").append(optionField);
 }
 
-// Function to remove an option field
 function removeOption(button) {
-    $(button).closest(".option-group").remove();
+  $(button).closest(".option-group").remove();
 }
